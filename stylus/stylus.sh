02cfc275af9676543d7e4d88ac300fbc465ee927
takayasumorishita <c011729632@edu.teu.ac.jp>
2021-01-31 22:16:57 +0900

    Change hovered link color

--- stylus/stylus.sh
+++
@@ -1,6 +0,0 @@
-STYLEFILE='../style/style.css'
-TARGET="danime-dark.stylus"
-
-echo '@-moz-document url-prefix("https://anime.dmkt-sp.jp/animestore/") {' > $TARGET
-cat $STYLEFILE >> $TARGET
-echo '}' >> $TARGET
